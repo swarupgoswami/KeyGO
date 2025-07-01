089e4b97685087f51821ab02ea7e8f04c1ba1fd7
swarup Goswami <goswamiswarup97@gmail.com>
2025-07-01 11:03:00 +0530

    added other components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar.jsx'
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import CarDetails from './pages/CarDetails.jsx'
+import Car from './pages/Car.jsx'
+import MyBookings from './pages/MyBookings.jsx'
+import Footer from './components/Footer.jsx'
+
+
+
 function App() {
 
   const [ShowLogin,setShowLogin]=useState(false)
@@ -8,6 +16,16 @@ function App() {
   return (
     <>
       {!isOwnerPath && <Navbar  setShowLogin={setShowLogin}/>}
+
+
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path="/car-details/:id" element={<CarDetails/>} />
+        <Route path="/cars" element={<Car/>} />
+        <Route path="/my-bookings" element={<MyBookings/>}/>
+      </Routes>
+
+      {!isOwnerPath && <Footer/>}
     </>
   )
 }
