a2e29b02cb61cd367258a2243fae90bd5bdfe5c8
swarup Goswami <goswamiswarup97@gmail.com>
2025-07-01 11:05:31 +0530

    upgraded the home.jsx and added the carDetails.jsx

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
-
+import React, { use, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 const CarDetails = () => {
+
+  const {id} = useParams();
+  const navigate=useNavigate();
+  const [Car,setCar]=useState(null);
+  useEffect()
   return (
     <div>
       
