002ae5b23e2f44474674389d4777e64aba05036e
swarup Goswami <goswamiswarup97@gmail.com>
2025-07-19 23:48:43 +0530

    fixed a typo in navbarowner.jsx

--- client/src/components/owner/NavbarOwner.jsx
+++ client/src/components/owner/NavbarOwner.jsx
@@ -8,7 +8,7 @@ const NavbarOwner = () => {
 
 
   return (
-    <div className='flex itme-center justify-between px-6 md:px-10 py-4 text-gray-500 border-b border-bordercolor relative transition-all'>
+    <div className='flex item-center justify-between px-6 md:px-10 py-4 text-gray-500 border-b border-bordercolor relative transition-all'>
 
         <Link to='/'>
           <img src={assets.logo} alt="logo" className='h-7' />
