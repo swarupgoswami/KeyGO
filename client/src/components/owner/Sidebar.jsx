e0d43e96647e2d4aa62dd85f5210520fc40eb2fc
swarup Goswami <goswamiswarup97@gmail.com>
2025-08-19 10:47:28 +0530

    chnaged the max width

--- client/src/components/owner/Sidebar.jsx
+++ client/src/components/owner/Sidebar.jsx
@@ -17,7 +17,7 @@ const Sidebar = () => {
 
 
   return (
-    <div className='bg-yellow-500 relative min-h-screen md:flex flex-col items-center pt-8 max-w-13 md:max-w-60 w-full border-r border-bordercolor text-sm'>
+    <div className='bg-yellow-500 relative min-h-screen md:flex flex-col items-center pt-8 max-w-13 md:max-w-50 w-full border-r border-bordercolor text-sm'>
 
         <div className="group reltaive ">
             <label htmlFor="image">
