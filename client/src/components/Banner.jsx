0a04592cc7faf08de43c11f788aa83e6d2234fc1
swarup Goswami <goswamiswarup97@gmail.com>
2026-01-20 18:58:26 +0530

    updated the banner.jsx file

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -6,13 +6,13 @@ const Banner = () => {
     <div className='flex flex-col md:flex-row md:items-start items-center justify-between px-8 min-md:pl-14 pt-10 bg-gradient-to-r from-[#0558FE] to-[#A9CFFF] max-w-6xl mx-3 md:mx-auto rounded-2xl overflow-hidden'>
 
         <div className="text-white">
-            <h2 className='text-2xl font-medium'>DO YOU OWN A LUXURY CAR ?</h2>
-            <p className='mt-2 font-medium'>Monetize your vechile effortlessly by listing it on KeyGO</p>
-            <p className='max-w-130'>we take care of insurance , driver verification and secure payments-- so you can earn passive income, stress-free</p>
+            <h2 className='text-3xl font-medium'>DO YOU OWN A LUXURY CAR ?</h2>
+            <p className='mt-3 font-medium'>Monetize your vechile effortlessly by listing it on KeyGO</p>
+            <p className='max-w-144'>we take care of insurance , driver verification and secure payments-- so you can earn passive income, stress-free</p>
 
             {/* button  */}
 
-            <button className='px-6 py-2 bg-white hover:bg-slate-100 hover:translate-y-1 transition-all text-primary rounded-lg text-sm mt-4 cursor-pointer'>List your car</button>
+            <button className='px-6 py-2 bg-white hover:bg-slate-300 hover:translate-y-1 transition-all text-primary rounded-lg text-sm mt-4 cursor-pointer'>List your car</button>
         </div>
 
         <img src={assets.banner_car_image} alt="banner car image" className='max-h-45 mt-10' />
