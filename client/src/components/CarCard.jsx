cfa13b2468df405076426bf474c14240cd781f3f
swarup Goswami <goswamiswarup97@gmail.com>
2026-01-21 19:15:09 +0530

    updated the css of the carCard.jsx component

--- client/src/components/CarCard.jsx
+++ client/src/components/CarCard.jsx
@@ -13,7 +13,7 @@ const CarCard = ({car}) => {
         <div className="relative h-48 overflow-hidden">
            <img src={car.image} alt="car icon" className='w-full h-full object-cover transition-transform duration-500 group-hover:scale-105'/>
 
-           {car.isAvailable && <p className='absolute top-4 left-4 bg-primary/90 text-white text-xs px-2.5 py-1 rounded-full'>Available no</p>}
+           {car.isAvailable && <p className='absolute top-4 left-4 bg-primary/90 text-white text-xs px-3.5 py-1 rounded-full'>Available no</p>}
 
 
            <div className="absolute bottom-4 right-4 bg-black/80 backdrop-blur-sm text-white px-3 py-2 rounded-lg">
